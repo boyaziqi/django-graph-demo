a82bf4b0be881a5c9f9962a44278f077e6538073
boyaziqi <catsaction@126.com>
2019-09-05 15:42:37 +0800

    创建文章mutation

--- apps/articles/schema.py
+++ apps/articles/schema.py
@@ -1,9 +1,13 @@
+from django import forms
+import graphene
 from graphene import relay
 
 from graphene_django.types import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
+from graphene_django.rest_framework.mutation import SerializerMutation
 
 from apps.articles.models import Category, Article
+from apps.articles.serializers import CategorySerializer
 
 
 class CategoryType(DjangoObjectType):
@@ -30,8 +34,35 @@ class Query(object):
     article = relay.Node.Field(ArticleType)
     all_articles = DjangoFilterConnectionField(ArticleType)
 
+    def resolve_category(self, info, **kwargs):
+        print('ffffffffffff')
+        id = kwargs.get('id')
+        return Category.objects.get(pk=id)
+
     def resolve_all_categories(self, info, **kwargs):
         return Category.objects.all()
 
     def resolve_all_articles(self, info, **kwargs):
         return Article.objects.select_related('category').all()
+
+
+class CategoryInput(graphene.InputObjectType):
+    name = graphene.String()
+
+
+class CreateCategoryMutation(graphene.Mutation):
+    class Arguments:
+        input = CategoryInput(required=True)
+
+    ok = graphene.Boolean()
+    category = graphene.Field(CategoryType)
+
+    def mutate(self, info, input):
+        ok = True
+        category = Category(name=input.name)
+        category.save()
+        return CreateCategoryMutation(ok, category)
+
+
+class Mutation(graphene.ObjectType):
+    create_category = CreateCategoryMutation.Field()
--- graphql_django/schema.py
+++ graphql_django/schema.py
@@ -1,12 +1,11 @@
 import graphene
 
 from apps.articles.schema import Query as ArticleQuery
-
+from apps.articles.schema import Mutation as ArticleMutation
 
 
 class Query(ArticleQuery, graphene.ObjectType):
-    # This class will inherit from multiple Queries
-    # as we begin to add more apps to our project
     pass
 
-schema = graphene.Schema(query=Query)
+
+schema = graphene.Schema(query=Query, mutation=ArticleMutation)
